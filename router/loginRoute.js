c9530d8bcbec332879a905ff7114c8969bc1a8b4
Sagar SOnkar <sagarsonker56625@gmail.com>
2023-07-05 23:13:39 +0530

    START

--- router/loginRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-const router=express.Router()
-const loginControl=require('../controllers/loginControl')
-
-router.post("/login", loginControl.login);
-
-module.exports=router
