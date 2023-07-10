39072f2d2f6c440d8219a74ea6ddbc0c1ebe7906
Sagar SOnkar <sagarsonker56625@gmail.com>
2023-07-10 16:55:16 +0530

    setInterval

--- FRONTEND/loggedIn/loggedIn.js
+++ FRONTEND/loggedIn/loggedIn.js
@@ -16,6 +16,7 @@ send.addEventListener("click", async (e) => {
     const result = await axios.post("http://localhost:3000/postChat", {
       chat: chat.value,
     }, { headers: { Authorization: localStorage.getItem("token")}});
+    // location.reload()
     const row = document.createElement("tr");
     const data = document.createElement("td");
     const User=await axios.get(`http://localhost:3000/getUser/${result.data.UserId}`, { headers: { Authorization: localStorage.getItem("token")}})
@@ -35,29 +36,31 @@ window.addEventListener("DOMContentLoaded", async (e) => {
   if(!localStorage.getItem("message")){
     localStorage.setItem('message',JSON.stringify([]))
   }
-  try {
-    tbody.innerHTML = "";
-    const chat = await axios.get(`http://localhost:3000/getChat/${JSON.parse(localStorage.getItem('message'))[-1]}`, {headers: {Authorization: localStorage.getItem("token")}})
-    if(chat){
-      message=JSON.parse(localStorage.getItem('message'))
-      message=message.concat(chat.data)
-      message=message.slice(-11)
-      localStorage.setItem('message',JSON.stringify(message))
+  setInterval(async ()=>{
+    try {
+      tbody.innerHTML = "";
+      const chat = await axios.get(`http://localhost:3000/getChat/${JSON.parse(localStorage.getItem('message'))[-1]}`, {headers: {Authorization: localStorage.getItem("token")}})
+      if(chat){
+        message=JSON.parse(localStorage.getItem('message'))
+        message=message.concat(chat.data)
+        message=message.slice(-11)
+        localStorage.setItem('message',JSON.stringify(message))
+      }
+      array=JSON.parse(localStorage.getItem('message'))
+      for(element of array){
+        const row = document.createElement("tr");
+        const data = document.createElement("td");
+        const User=await axios.get(`http://localhost:3000/getUser/${element.UserId}`, { headers: { Authorization: localStorage.getItem("token")}})
+        data.appendChild(document.createTextNode(`${User.data.first} ${User.data.sur}: ` + element.chat));
+        row.appendChild(data);
+        tbody.appendChild(row);
+      }
+      
+    } catch (err) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("message");
+      location.replace("http://localhost:5500/FRONTEND/logIn/login.html");
+      console.log(err);
     }
-    array=JSON.parse(localStorage.getItem('message'))
-    for(element of array){
-      const row = document.createElement("tr");
-      const data = document.createElement("td");
-      const User=await axios.get(`http://localhost:3000/getUser/${element.UserId}`, { headers: { Authorization: localStorage.getItem("token")}})
-      data.appendChild(document.createTextNode(`${User.data.first} ${User.data.sur}: ` + element.chat));
-      row.appendChild(data);
-      tbody.appendChild(row);
-    }
-    
-  } catch (err) {
-    localStorage.removeItem("token");
-    localStorage.removeItem("message");
-    location.replace("http://localhost:5500/FRONTEND/logIn/login.html");
-    console.log(err);
-  }
+  },1000)
 });
